af693fbbba2077ad39af12f6fae4dca3615959dd
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-06-09 17:47:48 +0300

    Update App Component to use the router links

--- photo-gallery/src/App.jsx
+++ photo-gallery/src/App.jsx
@@ -1,5 +1,36 @@
 import React from "react";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import { SharedLayout } from "./layouts/SharedLayout";
 
-export const App = () => {
-  return <div className="App"></div>;
+import { Home } from "./pages/Home";
+import { SignUp } from "./pages/SignUp";
+import { SignIn } from "./pages/SignIn";
+import { Favorites } from "./pages/Favorites";
+
+const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="favorites" element={<Favorites />} />
+        </Route>
+        <Route path="signup" element={<SignUp />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="*" />
+      </>
+    )
+  );
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 };
+
+export default App;
