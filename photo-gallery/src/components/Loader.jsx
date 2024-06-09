ddadbcc53ec269e8415fd5392f66bab528f19ee1
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-06-09 17:42:06 +0300

    Add Loader Component

---
+++ photo-gallery/src/components/Loader.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Bars } from "react-loader-spinner";
+import { Box } from "@mui/material";
+
+export function Loader() {
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
+      <Bars
+        height="80"
+        width="80"
+        color="#4fa94d"
+        ariaLabel="bars-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </Box>
+  );
+}
