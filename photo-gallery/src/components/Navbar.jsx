0188e3d52a6b52c1f71e71ceaa131c30275480c3
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-06-10 16:39:16 +0300

    Update Navbar Componeent by changing the behavior of showing buttons to show sign-up and sign-in if there is no user logged in and show user full name if the user is logged in

--- photo-gallery/src/components/Navbar.jsx
+++ photo-gallery/src/components/Navbar.jsx
@@ -1,33 +1,86 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 import { Link } from "react-router-dom";
+import { useUser } from "../contexts/UserContext";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LogoutIcon from "@mui/icons-material/Logout";
 
+// Create a navbar component to show the logo and show the sign-up and sign-in buttons if the user isn't logged in
+// and show the user full name with a dropdown menu containing the favorites and logout button if the use logged in
 export const Navbar = () => {
+  const { user, logout } = useUser();
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <>
-      <AppBar position="static">
-        <Toolbar>
-          <Box display="flex" alignItems="center" flexGrow={1}>
-            <Typography
-              variant="h6"
-              component={Link}
-              to="/"
-              style={{
-                textDecoration: "none",
-                color: "inherit",
-                marginRight: "16px",
-              }}
-            >
-              Pexels Photo Gallery
-            </Typography>
-            <Button color="inherit" component={Link} to="/">
-              Home
-            </Button>
-          </Box>
+    <AppBar position="static">
+      <Toolbar>
+        <Box display="flex" alignItems="center" flexGrow={1}>
+          <Typography variant="h6" sx={{ mr: 2 }}>
+            Pexels Photo Gallery
+          </Typography>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+        </Box>
+        {user ? (
           <Box>
-            <Button color="inherit" component={Link} to="/favorites">
-              Favorites
+            <Button color="inherit" onClick={handleMenu}>
+              {user.firstName} {user.lastName}
             </Button>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem component={Link} to="/favorites" onClick={handleClose}>
+                <ListItemIcon>
+                  <FavoriteIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText primary="Favorites" />
+              </MenuItem>
+              <MenuItem
+                onClick={() => {
+                  logout();
+                  handleClose();
+                }}
+              >
+                <ListItemIcon>
+                  <LogoutIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText primary="Logout" />
+              </MenuItem>
+            </Menu>
+          </Box>
+        ) : (
+          <Box>
             <Button color="inherit" component={Link} to="/signup">
               Sign Up
             </Button>
@@ -35,8 +88,10 @@ export const Navbar = () => {
               Sign In
             </Button>
           </Box>
-        </Toolbar>
-      </AppBar>
-    </>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 };
+
+export default Navbar;
