9052c3ab8dc8be88d2ecc928380be025a9ef67f5
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-06-10 17:15:03 +0300

    Update firebase config file to hide teh apikey in environment variables file

--- photo-gallery/src/config/firebase.js
+++ photo-gallery/src/config/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAtFLzLW0QIKdholO_DDEMcvbYzKRrDgSM",
-  authDomain: "pexels-photo-gallery.firebaseapp.com",
-  projectId: "pexels-photo-gallery",
-  storageBucket: "pexels-photo-gallery.appspot.com",
-  messagingSenderId: "692944518232",
-  appId: "1:692944518232:web:cc029be713d33e42591e2d",
-  measurementId: "G-D337WV44HL",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
