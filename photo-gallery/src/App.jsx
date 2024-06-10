58619cb53c5677f90f5b203a4366c19592294a2d
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-06-10 16:40:56 +0300

    Update App Component by adding a protection on a favorite route to require authentication and add the user provider to wrap the App Component

--- photo-gallery/src/App.jsx
+++ photo-gallery/src/App.jsx
@@ -1,35 +1,53 @@
 import React from "react";
 import {
+  Navigate,
   Route,
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import { SharedLayout } from "./layouts/SharedLayout";
-
 import { Home } from "./pages/Home";
 import { SignUp } from "./pages/SignUp";
 import { SignIn } from "./pages/SignIn";
 import { Favorites } from "./pages/Favorites";
+import { NotFound } from "./pages/NotFound";
+import { UserProvider, useUser } from "./contexts/UserContext";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
+  const RequireAuth = ({ children }) => {
+    const { user } = useUser();
+
+    return user ? children : <Navigate to="/signin" />;
+  };
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
-          <Route path="favorites" element={<Favorites />} />
+          <Route
+            path="favorites"
+            element={
+              <RequireAuth>
+                <Favorites />
+              </RequireAuth>
+            }
+          />
         </Route>
         <Route path="signup" element={<SignUp />} />
         <Route path="signin" element={<SignIn />} />
-        <Route path="*" />
+        <Route path="*" element={<NotFound />} />
       </>
     )
   );
   return (
-    <>
+    <UserProvider>
       <RouterProvider router={router} />
-    </>
+      <ToastContainer />
+    </UserProvider>
   );
 };
 
