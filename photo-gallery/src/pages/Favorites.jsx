8719edb562e2ff4a01abc11d1d7a1fa5203e4331
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-06-10 17:03:07 +0300

    Update Favorites Page to split into FavoriteGallery Component

--- photo-gallery/src/pages/Favorites.jsx
+++ photo-gallery/src/pages/Favorites.jsx
@@ -1,31 +1,70 @@
-import React, { useState } from "react";
-import { Grid, Card, CardMedia, Typography } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { Typography } from "@mui/material";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { db } from "../config/firebase";
+import { useUser } from "../contexts/UserContext";
+import { Loader } from "../components/Loader";
+import { FavoriteGallery } from "../components/FavoriteGallery";
 
 export const Favorites = () => {
-  const [favorites, setFavorites] = useState([
-    "https://images.pexels.com/photos/1234567/pexels-photo-1234567.jpeg",
-    "https://images.pexels.com/photos/25772450/pexels-photo-25772450.jpeg",
-  ]);
+  const { user } = useUser();
+  const [favorites, setFavorites] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const pageSize = 12;
+
+  const fetchFavorites = async () => {
+    setLoading(true);
+    const userDoc = await getDoc(doc(db, "Users", user.uid));
+    setTimeout(() => {
+      setFavorites(userDoc.data().favorites || []);
+      setLoading(false);
+    }, 500);
+  };
+
+  useEffect(() => {
+    if (user) {
+      fetchFavorites();
+    }
+  }, [user]);
+
+  const removeFavorite = async (index) => {
+    const updatedFavorites = favorites.filter((pre, i) => i !== index);
+    setFavorites(updatedFavorites);
+    await updateDoc(doc(db, "Users", user.uid), {
+      favorites: updatedFavorites,
+    });
+  };
+
+  const handlePrev = () => {
+    setPage((prev) => Math.max(prev - 1, 1));
+  };
+
+  const handleNext = () => {
+    setPage((prev) => (prev * pageSize < favorites.length ? prev + 1 : prev));
+  };
 
   return (
     <>
-      <Typography variant="h4" gutterBottom>
-        Favorites
-      </Typography>
-      <Grid container spacing={2}>
-        {favorites.map((photoUrl, index) => (
-          <Grid item key={index} xs={12} sm={6} md={4}>
-            <Card>
-              <CardMedia
-                component="img"
-                height="300"
-                image={photoUrl}
-                alt="Favorite"
-              />
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Typography variant="h4" gutterBottom>
+            Favorites
+          </Typography>
+          <FavoriteGallery
+            favorites={favorites}
+            page={page}
+            pageSize={pageSize}
+            handlePrev={handlePrev}
+            handleNext={handleNext}
+            removeFavorite={removeFavorite}
+          />
+        </>
+      )}
     </>
   );
 };
+
+export default Favorites;
