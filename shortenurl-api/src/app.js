a3ea3ab1b85c6c1ec2a66ef78d4c5070e5baed86
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-06-11 05:35:17 +0300

    Update app.js file

--- shortenurl-api/src/app.js
+++ shortenurl-api/src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { errorMiddleware } from "./middlewares/error.middleware.js";
 import { notFound } from "./middlewares/not-found.middleware.js";
+import routes from "./routes/index.js";
 
 dotenv.config();
 const app = express();
@@ -13,15 +14,13 @@ app.listen(port, () => {
   console.log(`Node.js API Listening on port: ${port}....`);
 });
 
-app.use("/api/", (req, res) => {
-  res.send("Welcome to the Node.js API");
-});
-
 // CORS Middleware
 app.use(cors());
 
 // Body Parser Middleware
 app.use(express.json());
+
+app.use("/api", routes);
 
 app.use("/", (req, res) => {
   res.send("Welcome to the Node.js API");
